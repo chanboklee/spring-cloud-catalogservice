2bae69a6036b78ba3af466d559bef18921ec3a1b
chanboklee <chanboklee@naver.com>
2022-12-28 01:43:15 +0900

    Spring-Cloud-CatalogService init commit..

---
+++ src/main/java/com/lee/catalogservice/domain/CatalogEntity.java
@@ -0,0 +1,32 @@
+package com.lee.catalogservice.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "catalog")
+@Entity
+public class CatalogEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+    @Column(nullable = false)
+    private String productName;
+    @Column(nullable = false)
+    private Integer stock;
+    @Column(nullable = false)
+    private Integer unitPrice;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    @ColumnDefault(value = "CURRENT_TIMESTAMP")
+    private LocalDateTime createAt;
+}
---
+++ src/main/java/com/lee/catalogservice/repository/CatalogRepository.java
@@ -0,0 +1,8 @@
+package com.lee.catalogservice.repository;
+
+import com.lee.catalogservice.domain.CatalogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CatalogRepository extends JpaRepository<CatalogEntity, Long> {
+    CatalogEntity findByProductId(String productId);
+}
